4e0d8e1d702d062ef6e0d32953c1671ca4a96c87
Alanne Soares <alannesilva@yahoo.com.br>
2020-04-05 18:15:01 -0300

    Dados alterados

--- src/com/company/ConnectDB.java
+++ src/com/company/ConnectDB.java
@@ -14,29 +14,13 @@ class ConnectDB {
 
         cdb = new ConnectDataBase();
 
-        cdb.setQuery("with parm as" +
-                "(select null as matricula, null as cpf, 'Alanne Soares' as nome from dual" +
-                "select  VF.NMFUNCIONARIO,VF.CPF,VF.CDMATRICULA" +
-                "from MPRJ.MPRJ_VW_FUNCIONARIO vf" +
-                "inner join parm on 1=1" +
-                "where VF.CARGO = 'PROCURADOR'" +
-                "and     VF.CDSITUACAOFUNC = '1'" +
-
-                "/* se matrícula */" +
-                "and VF.CDMATRICULA = nvl(parm.matricula,VF.CDMATRICULA)" +
-
-                "/* se cpf */" +
-                "and VF.CPF = nvl (parm.cpf,VF.CPF)" +
-
-                "/* se nome */" +
-                "and VF.NMFUNCIONARIO like '%'||nvl(parm.nome,VF.NMFUNCIONARIO)||'%'" +
-                "order by 1,2");
+        cdb.setQuery("");
 
         // Drive
         cdb.setDrive("oracle.jdbc.driver.OracleDriver");
-        cdb.setUrl("jdbc:oracle:thin:@(DESCRIPTION=(LOAD_BALANCE=on)(ADDRESS = (PROTOCOL=TCP)(HOST=exa-scan.pgj.rj.gov.br)(PORT=1521))(CONNECT_DATA = (SERVICE_NAME=CORR)))");
-        cdb.setUsername("TJRJ_WEBSERVICE_CON");
-        cdb.setPassword("TJRJ_WEBSERVICE_CON");
+        cdb.setUrl("jdbc:oracle:thin:@(DESCRIPTION=(LOAD_BALANCE=on)(ADDRESS = (PROTOCOL=TCP)(HOST=)(PORT=))(CONNECT_DATA = (SERVICE_NAME=CORR)))");
+        cdb.setUsername("");
+        cdb.setPassword("");
 
 
 
